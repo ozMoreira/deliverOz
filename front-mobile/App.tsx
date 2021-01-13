c5456c430fe62fde5076f52bff68b07ab69e49f7
unknown <oz.moreira@icloud.com>
2021-01-13 07:40:38 -0300

    Add home page

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -1,12 +1,30 @@
+import{ useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans';
+//import { useFonts, Lobster_400Regular } from '@expo-google-fonts/lobster';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './src/Header';
+import AppLoading from 'expo-app-loading';
+import Home from './src/Home';
+
+
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    //Lobster_400Regular,
+    OpenSans_400Regular,
+    OpenSans_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Header />
+      <Home />
     </View>
   );
 }
@@ -15,7 +33,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    //alignItems: 'center',
+    //justifyContent: 'center',
   },
 });
